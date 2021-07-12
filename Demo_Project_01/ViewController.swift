8da50b9433ea5ca783dcd0ac09fd710e3dfee4c2
Quy <quyhd@mcrew-tech.com>
2021-07-12 18:02:36 +0700

    Initial Commit

---
+++ Demo_Project_01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo_Project_01
+//
+//  Created by Mcrew Tech on 12/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Demo_Project_01Tests/Demo_Project_01Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Demo_Project_01Tests.swift
+//  Demo_Project_01Tests
+//
+//  Created by Mcrew Tech on 12/07/2021.
+//
+
+import XCTest
+@testable import Demo_Project_01
+
+class Demo_Project_01Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
